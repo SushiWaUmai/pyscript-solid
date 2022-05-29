ec437a0c80cce1971da9cc9affd2304be5d88745
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-29 20:24:21 +0200

    perf: :zap: remove redundent packages

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Pyscript Solid
-
 Write pyscript code in solidjs.
+
+## Getting Started
+Note: This library currently only supports solidjs using vite.
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,1 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
-import App from './App';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
+export * from "./components";
