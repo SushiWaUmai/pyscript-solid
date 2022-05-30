71b53b8090fd1041fbd6fc221002ab5598feb2fe
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-30 21:58:30 +0200

    test: :test_tube: add failling tests

--- test/PyScript.test.tsx
+++ test/PyScript.test.tsx
@@ -1,9 +1,25 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render } from "solid-testing-library";
 import { PyScript } from "../src";
 
 describe("<PyScript />", () => {
   test("renders", () => {
-    render(() => <PyScript />);
+    const { unmount, container } = render(() => <PyScript />);
+    expect(container.hasAttribute("src")).toBe(false);
+    unmount();
+  });
+
+  test("has attributes", () => {
+    const { unmount, container } = render(() => <PyScript src="test.py" />);
+    expect(container.getAttribute("src")).toBe("test.py");
+    unmount();
+  });
+
+  test("has children", () => {
+    const { unmount, container } = render(() => (
+      <PyScript>print("Hello World!")</PyScript>
+    ));
+    expect(container.textContent).toBe(`print("Hello World!")`);
+    unmount();
   });
 });
