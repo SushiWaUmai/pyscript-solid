8f349bbe631749bedf16f87fc867e4a3ae403e24
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-30 21:47:33 +0200

    test: :white_check_mark: add first test

--- test/PyScript.test.tsx
+++ test/PyScript.test.tsx
@@ -1,10 +1,9 @@
-import { describe, expect, test } from "vitest";
+import { describe, test } from "vitest";
 import { render } from "solid-testing-library";
-import { PyScript } from "../src/components/PyScript";
+import { PyScript } from "../src";
 
 describe("<PyScript />", () => {
   test("renders", () => {
-    const { baseElement, unmount } = render(() => <PyScript />);
-    unmount();
+    render(() => <PyScript />);
   });
 });
